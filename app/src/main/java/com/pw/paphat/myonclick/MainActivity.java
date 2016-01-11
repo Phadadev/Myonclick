03a256ca38cd72dad499d613403c9d4b8bb9b4ef
NYR <paphat.dev@gmail.com>
2016-01-11 14:34:53 +0700

    start

---
+++ app/src/main/java/com/pw/paphat/myonclick/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pw.paphat.myonclick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
